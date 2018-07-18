4bb3628f23aa0bec94bf788c7e2d6c2f6a3c7e3c
Anders Lybecker <anders@lybecker.com>
2018-07-18 11:07:53 +0200

    A working Hello World server  - TCP based

---
+++ DotNettyPlayground/HelloWorldServer/HelloWorldServerHandler.cs
@@ -0,0 +1,32 @@
+﻿using DotNetty.Common.Internal.Logging;
+using DotNetty.Transport.Channels;
+using System;
+using System.Net;
+
+namespace HelloWorldServer
+{
+    public class HelloWorldServerHandler : SimpleChannelInboundHandler<string>
+    {
+        static readonly IInternalLogger Logger = InternalLoggerFactory.GetInstance<HelloWorldServerHandler>();
+
+        public override void ChannelActive(IChannelHandlerContext contex)
+        {
+            contex.WriteAsync($"Welcome to {Dns.GetHostName()} !\r\n");
+            contex.WriteAsync($"It is {DateTime.Now} now !\r\n");
+            contex.WriteAndFlushAsync("Write your name: ");
+        }
+        protected override void ChannelRead0(IChannelHandlerContext ctx, string msg)
+        {
+            Logger.Info("Received message: " + msg);
+            ctx.WriteAndFlushAsync($"Hello, {msg}\r\n");
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext contex, Exception e)
+        {
+            Logger.Error(e);
+            contex.CloseAsync();
+        }
+
+        public override bool IsSharable => true;
+    }
+}
---
+++ DotNettyPlayground/HelloWorldServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using DotNetty.Codecs;
+using DotNetty.Common.Internal.Logging;
+using DotNetty.Handlers.Logging;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using Microsoft.Extensions.Logging.Console;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloWorldServer
+{
+    class Program
+    {
+        static async Task RunServerAsync()
+        {
+            var logLevel = LogLevel.INFO;
+            InternalLoggerFactory.DefaultFactory.AddProvider(new ConsoleLoggerProvider((s, level) => true, false));
+
+            var serverPort = 8080;
+
+            var bossGroup = new MultithreadEventLoopGroup(1); //  accepts an incoming connection
+            var workerGroup = new MultithreadEventLoopGroup(); // handles the traffic of the accepted connection once the boss accepts the connection and registers the accepted connection to the worker
+
+            var encoder = new StringEncoder();
+            var decoder = new StringDecoder();
+            var serverHandler = new HelloWorldServerHandler();
+
+            try
+            {
+
+                var bootstrap = new ServerBootstrap(); // Bootstrap only has one group
+
+                bootstrap
+                    .Group(bossGroup, workerGroup)
+                    .Channel<TcpServerSocketChannel>()
+                    .Option(ChannelOption.SoBacklog, 100) // maximum queue length for incoming connection
+                    .Handler(new LoggingHandler(logLevel))
+                    .ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+
+                        // Do not allow requests longer than n chars
+                        pipeline.AddLast(new DelimiterBasedFrameDecoder(8192, Delimiters.LineDelimiter()));
+                        pipeline.AddLast(encoder, decoder, serverHandler);
+                    }));
+
+
+                
+                IChannel bootstrapChannel = await bootstrap.BindAsync(serverPort);
+
+                Console.WriteLine("Let us test the server in a command prompt");
+                Console.WriteLine($"\n telnet localhost {serverPort}");
+                Console.ReadLine();
+
+                await bootstrapChannel.CloseAsync();
+            }
+            finally
+            {
+                Task.WaitAll(bossGroup.ShutdownGracefullyAsync(), workerGroup.ShutdownGracefullyAsync());
+            }
+        }
+
+        static void Main(string[] args) => RunServerAsync().Wait();
+    }
+}
