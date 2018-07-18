627a74bca7aab00efd8e8dba138e74df3ac5d160
Anders Lybecker <anders@lybecker.com>
2018-07-18 20:27:57 +0200

    Made the server output on the client blue to highlight the data

--- Poco.Client/PocoClientHandler.cs
+++ Poco.Client/PocoClientHandler.cs
@@ -8,15 +8,19 @@ namespace Poco.Client
     {
         protected override void ChannelRead0(IChannelHandlerContext contex, Person person)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Data returned from Server:");
             Console.WriteLine(person.ToString());
+            Console.ResetColor();
         }
 
         public override void ExceptionCaught(IChannelHandlerContext contex, Exception e)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(DateTime.Now.Millisecond);
             Console.WriteLine("{0}", e.StackTrace);
             contex.CloseAsync();
+            Console.ResetColor();
         }
     }
 }
