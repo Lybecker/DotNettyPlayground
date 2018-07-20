0c2c2c9029928241cf400f228fea93c0678b1143
Anders Lybecker <anders@lybecker.com>
2018-07-20 16:03:41 +0200

    Echo handler greats new connections

--- MultipleProtocols.Server/EchoConnectionHandler.cs
+++ MultipleProtocols.Server/EchoConnectionHandler.cs
@@ -7,6 +7,12 @@ namespace MultipleProtocols.Server
     {
         static readonly IInternalLogger Logger = InternalLoggerFactory.GetInstance<EchoConnectionHandler>();
 
+        public override void ChannelActive(IChannelHandlerContext ctx)
+        {
+            ctx.WriteAsync("Hello,\r\n");
+            ctx.WriteAndFlushAsync("Write anything and the server will return the message\r\n");
+        }
+
         protected override void ChannelRead0(IChannelHandlerContext ctx, string msg)
         {
             ctx.WriteAsync($"You send me: '{msg}'\r\n");
